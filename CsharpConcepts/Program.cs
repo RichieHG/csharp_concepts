40d152d741ce33c29c893621536a98e0c4d3cb9f
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-10-10 18:33:54 -0500

    Feature: Add an example to use Virtual and Override modifiers

--- CsharpConcepts/Program.cs
+++ CsharpConcepts/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using CsharpConcepts.VirtualAndOverride;
+using System;
 
 namespace CsharpConcepts
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double taxiCost = 12.5, busCost = 10.0, subwayCost = 5.0;
+            double busCostPerKm = 2.7, taxiCostPerKm = 1.85, taxiCostPerMinute = 0.75;
+            double tripDistanceInKm = 8.6, tripTimeInMinutes = 30;
+            string route = "Metro Muzquiz - Metro Oceanía";
+
+            Bus busTrip = new Bus(busCost, busCostPerKm, tripDistanceInKm);
+            busTrip.Route = route;
+            Subway subwayTrip = new Subway(subwayCost);
+            subwayTrip.Route = route;
+            Taxi taxiTrip = new Taxi(taxiCost, taxiCostPerKm, tripDistanceInKm, taxiCostPerMinute, tripTimeInMinutes);
+            taxiTrip.Route = null;
+            Console.WriteLine("Bus trip info: Route {0} | Cost: {1:F2}", busTrip.Route, busTrip.Charge());
+            Console.WriteLine("Subway trip info: Route {0} | Cost: {1:F2}", subwayTrip.Route, subwayTrip.Charge());
+            Console.WriteLine("Taxi trip info: Route {0} | Cost: {1:F2}", taxiTrip.Route, taxiTrip.Charge());
+
         }
     }
+    
 }
---
+++ CsharpConcepts/VirtualAndOverride/PublicTransport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpConcepts.VirtualAndOverride
+{
+    public class PublicTransport
+    {
+        public double Cost { get; set; }
+        public virtual string Route { get; set; }
+        public virtual double CostPerKm { get; set; }
+        public virtual double CostPerMinute { get; set; }
+        public virtual double TripDistanceInKm { get; set; }
+        public virtual double TripTimeInMinutes { get; set; }
+        public PublicTransport()
+        {
+        }
+
+        public PublicTransport(double cost, double costPerKm, double tripDistanceInKm, double costPerMinute, double tripTimeInMinutes)
+        {
+            Cost = cost;
+            CostPerKm = costPerKm;
+            TripDistanceInKm = tripDistanceInKm;
+            CostPerMinute = costPerMinute;
+            TripTimeInMinutes = tripTimeInMinutes;
+        }
+
+        public virtual double Charge()
+        {
+            return Cost;
+        }
+    }
+}
