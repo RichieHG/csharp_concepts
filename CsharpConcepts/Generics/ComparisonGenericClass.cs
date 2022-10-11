709c8f65dc921fd146dfcf0c1c1d4cc4ed6072f9
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-10-11 11:24:21 -0500

    Feat: Add example of Generic class

---
+++ CsharpConcepts/Generics/ComparisonGenericClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpConcepts.Generics
+{
+    public class ComparisonGenericClass<T>
+    {
+
+        public bool CompareValues(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        public void ShowWelcomeMessage()
+        {
+            Console.WriteLine("Hi, this is a class to compare this type of data: {0}", typeof(T).ToString());
+        }
+    }
+}
